1819b5a7b7db4cd66ccec89258317b80cccbb438
kamster94 <kamster94@gmail.com>
2023-04-09 20:08:51 +0200

    Added Tailwind and Flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable global-require */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './node_modules/flowbite-react/**/*.js',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+};
